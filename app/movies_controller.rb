7de93ac14df72e967f1e9f924e752e82d88c29b5
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-15 22:19:27 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,7 +46,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(:id)
+  Movie.find()
 end
 
 def can_find_by_multiple_attributes
@@ -54,7 +54,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by()
+  Movie.find_by(title: "Title")
 end
 
 def can_find_using_where_clause_and_be_sorted
